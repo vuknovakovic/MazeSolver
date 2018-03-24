2bcb849706ceab360031254b91c9256384c688a6
Vuk Novakovic <vnovakovic96@gmail.com>
2018-03-24 18:45:17 +0100

    minor changes

--- src/main.py
+++ src/main.py
@@ -20,14 +20,16 @@ if not os.path.isdir("../output"):
 
 
 
-os.system("python2 ./maze_generator.py 300 500 " + name + " spiral")
+#os.system("python2 ./maze_generator.py 2000 2000 " + name + " perfect")
+#print("Generated image: " + name)
 
 pic_path="../input/" + name 
 
 
-start_time=time.time()
 
+print("Generating graph")
 maze=Maze(pic_path)
+print("Graph generated\nNumber of nodes: %d" % len(maze.adj_list))
 
 try:
     solve_method=getattr(maze, solver)
@@ -38,9 +40,15 @@ except AttributeError:
 pic_res=maze.img.img
 pic_res = cv.cvtColor(pic_res, cv.COLOR_GRAY2BGR)
 
+print("Starting solver: " + solver)
+start_time=time.time()
+
 solution=solve_method(maze.img.start_string, maze.img.finish_string)
+
 length=len(solution)
 color=0
+
+print("Generetaing result image")
 
 for s in range(0,length-1):
     curr=solution[s].split("_")
@@ -49,7 +57,7 @@ for s in range(0,length-1):
     curr=[int(x) for x in curr]
     nxt=[int(x) for x in nxt]
     
-    color_to_apply=[color, (255-color)/2 , 255-color] #set any color you like
+    color_to_apply=[0, 0 , 255-color] #set any color you like
 
     if curr[0]==nxt[0]:
         for x in range(min(curr[1], nxt[1]), max(curr[1], nxt[1])):
@@ -62,7 +70,7 @@ for s in range(0,length-1):
 
 end_time=time.time()
 
-print("{}".format(solver) + "\nPath length: " + str(length) + "\nRuntime: %g s" % (end_time - start_time))
+print("Path length: " + str(length) + "\nRuntime: %g s" % (end_time - start_time))
 name_no_ext=name[0:name.find(".")]
 
 
